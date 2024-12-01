161dafd74474adc634631f7e6cf136a891a8018c
Michal Templin <michaltemplin@surferseo.com>
2024-12-01 18:58:22 +0100

    feat: don't override already created init_day files, sync gh branch when initializing a day

--- bin/init_day.sh
+++ bin/init_day.sh
@@ -1,12 +1,29 @@
 
 # make a new directory for the today or passed day
-day=${1:-$(date +'%d')}
-mkdir lib/src/day$day
-# copy templates
-cp lib/template/day_X.ex          lib/src/day$day/day_$day.ex
-cp lib/template/day_X.test.exs    lib/src/day$day/day_$day.test.exs
+day_number=${1:-$(date +'%d')}
+day=day_$day_number
 
-# replace placeholder day value with the actual day
-placeholder='__DAY__'
-sed -i -e "s/$placeholder/$day/g" lib/src/day$day/day_$day.ex
-sed -i -e "s/$placeholder/$day/g" lib/src/day$day/day_$day.test.exs
+if git rev-parse --verify $day
+then
+  git checkout $day
+else
+  git checkout master
+  git pull
+  git checkout -b $day
+fi;
+
+if [ -f lib/src/$day/$day.ex ]
+then
+  echo "already initialized, skipping"
+else
+  # copy templates
+  mkdir lib/src/$day
+
+  cp lib/template/puzzle_template.txt       lib/src/$day/$day.ex
+  cp lib/template/puzzle_test_template.txt  lib/src/$day/$day.test.exs
+
+  # replace placeholder day value with the actual day
+  placeholder='__DAY__'
+  sed -i -e "s/$placeholder/$day_number/g" lib/src/$day/$day.ex
+  sed -i -e "s/$placeholder/$day_number/g" lib/src/$day/$day.test.exs
+fi;
