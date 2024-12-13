4029105da62f454c317278eb75a223bf33714545
Michal Templin <michaltemplin@surferseo.com>
2024-12-13 13:53:17 +0100

    feat: day12 part2

--- lib/src/day_12/day_12.ex
+++ lib/src/day_12/day_12.ex
@@ -5,30 +5,44 @@ defmodule Aoc.Day12 do
   """
   def execute_part_1(data \\ fetch_data()) do
     data
-    |> parse_input()
-    |> with_plot_coordinates()
-    |> group_by_plant()
-    |> Enum.map(fn {_cell, plot_coordinates} ->
-      plot_coordinates
-      |> split_into_regions()
-      |> Enum.map(fn region ->
-        area = calculate_area(region)
+    |> as_grouped_regions()
+    |> Enum.map(fn {_cell, region} ->
+      area = calculate_area(region)
 
-        region
-        |> get_fence_coordinates()
-        |> calculate_perimeter()
-        |> calculate_cost(area)
-      end)
-      |> Enum.sum()
+      region
+      |> get_fence_coordinates()
+      |> calculate_perimeter()
+      |> calculate_cost(area)
     end)
     |> Enum.sum()
   end
 
   def execute_part_2(data \\ fetch_data()) do
     data
-    |> parse_input()
+    |> as_grouped_regions()
+    |> Enum.map(fn {_cell, region} ->
+      area = calculate_area(region)
 
-    0
+      region
+      |> get_fence_coordinates()
+      |> List.flatten()
+      |> concat_adjoining_fences()
+      |> calculate_perimeter()
+      |> calculate_cost(area)
+    end)
+    |> Enum.sum()
+  end
+
+  defp as_grouped_regions(raw_data) do
+    raw_data
+    |> parse_input()
+    |> with_plot_coordinates()
+    |> group_by_plant()
+    |> Enum.flat_map(fn {cell, plot_coordinates} ->
+      plot_coordinates
+      |> split_into_regions()
+      |> Enum.map(&{cell, &1})
+    end)
   end
 
   defp split_into_regions(plot_coordinate, areas \\ [])
@@ -50,7 +64,7 @@ defmodule Aoc.Day12 do
     area_coordinates = Enum.concat(area_coordinates, current_coordinates)
 
     current_coordinates
-    |> Enum.flat_map(&get_neighbors(&1))
+    |> Enum.flat_map(&get_neighbors/1)
     |> Enum.uniq()
     |> Enum.reject(&(&1 in area_coordinates))
     |> Enum.filter(&(&1 in possible_coordinates))
@@ -60,8 +74,58 @@ defmodule Aoc.Day12 do
   defp get_fence_coordinates(area_coordinates) do
     Enum.map(area_coordinates, fn plot ->
       plot
-      |> get_neighbors()
-      |> Enum.reject(&(&1 in area_coordinates))
+      |> get_neighbors(with_sides: true)
+      |> Enum.reject(fn {x, y, _} -> {x, y} in area_coordinates end)
+    end)
+  end
+
+  defp concat_adjoining_fences(fence_coordinates) do
+    fence_coordinates
+    |> Enum.group_by(
+      fn
+        {x, _y, side} when side in [:left, :right] -> {nil, x, side}
+        {_x, y, side} when side in [:up, :down] -> {y, nil, side}
+      end,
+      fn
+        {_x, y, side} when side in [:left, :right] -> y
+        {x, _y, side} when side in [:up, :down] -> x
+      end
+      # fn
+      #   {_x, y, side} when side in [:left, :right] -> {nil, y, side}
+      #   {x, _y, side} when side in [:up, :down] -> {x, nil, side}
+      # end,
+      # fn
+      #   {x, _y, side} when side in [:left, :right] -> x
+      #   {_x, y, side} when side in [:up, :down] -> y
+      # end
+    )
+    |> Enum.flat_map(fn
+      {{x, nil, side}, coordinates} ->
+        coordinates
+        |> into_ranges()
+        |> Enum.map(&{x, &1, side})
+
+      {{nil, y, side}, coordinates} ->
+        coordinates
+        |> into_ranges()
+        |> Enum.map(&{&1, y, side})
+    end)
+  end
+
+  defp into_ranges(numbers) do
+    numbers
+    |> Enum.sort()
+    |> Enum.chunk_while(
+      [],
+      fn
+        elem, [] -> {:cont, [elem]}
+        elem, [last_elem | _] = acc when last_elem + 1 == elem -> {:cont, [elem | acc]}
+        elem, acc -> {:cont, acc, [elem]}
+      end,
+      fn acc -> {:cont, acc, nil} end
+    )
+    |> Enum.map(fn range ->
+      List.last(range)..List.first(range)
     end)
   end
 
@@ -71,14 +135,12 @@ defmodule Aoc.Day12 do
   defp calculate_area(region_coordinates), do: Enum.count(region_coordinates)
   defp calculate_cost(perimeter, area), do: perimeter * area
 
-  defp get_neighbors({x, y}) do
-    [
-      {x + 1, y},
-      {x - 1, y},
-      {x, y + 1},
-      {x, y - 1}
-    ]
-  end
+  defp get_neighbors(_point, opts \\ [])
+
+  defp get_neighbors({x, y}, with_sides: true),
+    do: [{x + 1, y, :right}, {x - 1, y, :left}, {x, y + 1, :down}, {x, y - 1, :up}]
+
+  defp get_neighbors({x, y}, _), do: [{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}]
 
   # helpers
   defp parse_input(input) do
--- lib/src/day_12/day_12.test.exs
+++ lib/src/day_12/day_12.test.exs
@@ -16,6 +16,12 @@ defmodule Aoc.Day12Test do
     assert result == 140
   end
 
+  test "execute_part_2/1 - data 1" do
+    result = Subject.execute_part_2(@test_data_1)
+
+    assert result == 80
+  end
+
   @test_data_2 """
   OOOOO
   OXOXO
@@ -49,9 +55,38 @@ defmodule Aoc.Day12Test do
     assert result == 1930
   end
 
-  test "execute_part_2/1" do
+  test "execute_part_2/1 - data 3" do
     result = Subject.execute_part_2(@test_data_3)
 
-    assert result == 0
+    assert result == 1206
+  end
+
+  @test_data_4 """
+  EEEEE
+  EXXXX
+  EEEEE
+  EXXXX
+  EEEEE
+  """
+
+  test "execute_part_2/1 - data 4" do
+    result = Subject.execute_part_2(@test_data_4)
+
+    assert result == 236
+  end
+
+  @test_data_4 """
+  AAAAAA
+  AAABBA
+  AAABBA
+  ABBAAA
+  ABBAAA
+  AAAAAA
+  """
+
+  test "execute_part_2/1 - data 5" do
+    result = Subject.execute_part_2(@test_data_4)
+
+    assert result == 368
   end
 end
